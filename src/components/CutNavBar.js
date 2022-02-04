ce40ee36d413753e4e4f58e805e1b4f0d00785a8
Randy <randyisbusy@gmail.com>
2022-02-03 22:17:24 -0700

    adds store hours and phone number

--- src/components/CutNavBar.js
+++ src/components/CutNavBar.js
@@ -8,7 +8,7 @@ const CutNavBar = ({darkMode}) =>{
     
         <span  id="navbar" className={`d-flex justify-content-center flex-row bg${darkMode ? '-dark' : '-white'} `}>
             <Link className="mx-3" key={uuid()} to="/CheckIn">Check In</Link>
-            <Link className="mx-3" key={uuid()} to='/Shop'>Shop</Link>
+            <Link className="mx-3" key={uuid()} to='/'>Shop</Link>
             <Link className="mx-3" key={uuid()} to='/CheckedIn'>Checked In</Link> 
             
         </span>
--- src/components/Index.js
+++ src/components/Index.js
@@ -65,8 +65,7 @@ const Index = () => {
               let time = 9 - now 
               openingIn(time)
             } 
-            else if (now > 17){
-                debugger 
+            else if (now > 17){ 
                let time =  24 - now + 9 
                openingIn(time)
             }  
@@ -82,7 +81,7 @@ const Index = () => {
       }  
     
       
-    return(<Row key={darkMode} className={`d-flex flex-row   ${ darkMode ? 'bg-dark' : 'bg-white'}bg-dark`}>  
+    return(<Row key={darkMode} className={`d-flex flex-row justify-content-center  ${ darkMode ? 'bg-dark' : 'bg-white'}bg-dark`}>  
     <Col className={`flex-column text-center justify-content-left mr-5 ${darkMode ? 'bg-dark' : 'bg-white'}`}> 
     <Button sm id="admin" className='d-flex justify-content-left btn-sm' onClick={(e)=>showAdmin(e)}>Admin?</Button>
     <form className={`d-${ adminDisplay ? 'flex' : 'none'} flex-row ${darkMode ? 'bg-dark' : 'bg-white'}`} 
@@ -91,12 +90,23 @@ const Index = () => {
     </form> 
     <Button key={words === '50$ifyouaint'} btn-sm className={`d-${ admin ? 'flex' : 'none'} ml-4 justify-content-left btn btn-xs ${darkMode ? 'bg-dark' : 'bg-white'}`} onClick={toggleMode}>{darkMode ? 'Light Mode' : 'Dark Mode'}</Button>
     <CutNavBar darkMode={darkMode} /> 
-    <div className={`${ darkMode ? 'bg-dark soSharpDark' : 'bg-white soSharpLite'} `}> 
+    <div className={`${ darkMode ? 'bg-dark soSharpDark' : 'd-flex flex-column justify-content-center mr-0 bg-white soSharpLite'} `}> 
 
     </div> 
     <div className="d-flex flex-column justify-content-center"> 
-      <h1 className={`${open ? 'open' : 'closed'}`}>{ open ? 'Open': 'Closed'}</h1> 
+      <h1 className={`${open ? 'open thin' : 'closed'}`}>{ open ? 'Open': 'Closed'}</h1> 
       <h4 className={`${ open ? 'd-none' : 'closed'}`} >Will open in { open ? '' : `${ willOpen }`} hours</h4>
+    </div> 
+    <div className='d-flex flex-column justify-content-center'>
+      <p>5630 W Camelback Rd UNIT 119, Glendale, AZ 85301</p> 
+      <p>(623) 937-4277</p>
+      <p>Monday: Closed</p> 
+      <p>Tuesday: 9AM-5PM</p> 
+      <p>Webnesday: 9AM-5PM</p>
+      <p>Thrusday: 9AM-5PM</p>
+      <p>Friday: 9AM-6PM</p>
+      <p>Saturday: 9AM-6PM</p> 
+      <p>Sunday: 11AM-3PM</p>
     </div>
     </Col>
     </Row>
